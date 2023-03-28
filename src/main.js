93008b6ddee1376cfa69c25ecb7fc5fccd9aac9c
Elisafa <elisafajunior@hotmail.com>
2023-03-28 19:32:16 -0300

    Finalizado cabeçlho

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
     const questions = document.querySelectorAll('[data-faq-question]')
+    
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+    window.addEventListener('scroll', function(){
+       const posicaoAtual = window.scrollY
+       if(posicaoAtual < alturaHero){
+            ocultaElementosDoHeader()
+       }else{
+            exibeElementosDoHeader()
+       }
+    })
 
-
-
+// Sessão de atrações - programação das abas
     buttons.forEach(item =>{
         item.addEventListener('click', (event) =>{
             const abaAlvo = event.target.dataset.tabButton
@@ -17,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     })
 
+    // Sessão perguntas e respostas
     questions.forEach(item => {
         item.addEventListener('click', abreOuFechaResposta)
     })
@@ -27,6 +38,16 @@ function abreOuFechaResposta(el){
     elPai.classList.toggle(classe)
 }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header')
+    header.classList.remove('header--is-hidden')
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]')
